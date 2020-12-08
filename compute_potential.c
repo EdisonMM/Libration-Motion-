bc01516072b984b28665ba582b2be0a5fc365b0e
Edison <emm712@nyu.edu>
2020-12-08 00:16:00 -0500

    Added Main Parameters m and M

--- compute_potential.c
+++ compute_potential.c
@@ -5,11 +5,34 @@
 #include "potential.h"
 #include "gradient.h"
 #include <math.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
+int main(int argc, char* argv[]){
 
     float t0, tmax, r0, phi0, p0, l0, m, M, R, G, h, xmin, xmax, ymin, ymax;
     int N;
+
+    m = 1;
+    M = 10;
+
+    char* str1 = "-m";
+    char* str2 = "-M";
+
+    for (int j = 0; j < argc; j++)
+    {
+        if (strcmp(argv[j], str1) == 0)
+        {
+
+            m = atof(argv[j+1]);
+        }
+
+        if (strcmp(argv[j], str2) == 0)
+        {
+            M = atof(argv[j+1]);
+        }
+
+    }
 
     t0 = 0;
     tmax = 100;
@@ -17,10 +40,10 @@ int main(){
     phi0 = 0;
     p0 = 1;
     l0 = 0;
-    m = 1;
-    M = 10;
     R = 1;
     G = 6.67e-11;
+
+
     //Number of time steps
     N = 1000;
     h = 0.2;
